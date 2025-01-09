1bc6dc4934961dcf48c0c221b95e9445be1555bd
Kolade-Amire <stephamire@gmail.com>
2025-01-09 21:10:07 +0100

    backend: update app with backend test config

--- backend/src/test/java/com/finapp/backend/BackendApplicationTest.java
+++ backend/src/test/java/com/finapp/backend/BackendApplicationTest.java
@@ -2,11 +2,13 @@ package com.finapp.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTest {
     @Test
     public void contextLoads() {
